e2b7fc11199677eca4d9b04ff8c24b94f8054746
rapha717 <andreyravens@gmail.com>
2016-04-03 16:12:27 -0300

    Update main.java

--- main.java
+++ main.java
@@ -7,29 +7,23 @@ import java.net.URL;
 /*
 PROJETO INTEGRADOR 
 
-PARA FAZER:
-IMPLEMENTAR ESTADOS DE JOGO
-IMPLEMENTAR PLATAFORMA
-IMPLEMENTAR ALVO
-IMPLEMENTAR BARRA DE FORCA
-IMPLEMENTAR TODO O RESTO
+PRIMEIRA VERSAO FUNCIONAL
+
 */
 
 
 public class main {
     
-    public static float fps = 60;
+    public static float fps = 50;
     public static float calculos[] = {1, 30, 1, 30, 60, 1, 50};
+    public static boolean b = false;
 
     
     public static void main(String[] args) {
-
+                
         main janela = new main();
         janela.cria_janela();
-        
     }
-    
-
     
     public static float[] calcular_movimento(float[] entrada) {
         float x, x_zero, y, y_zero, v, t, angulo;
@@ -40,7 +34,6 @@ public class main {
         v = entrada[4];
         t = entrada[5];
         angulo = entrada[6];
-        
         
         t = t + (1f / fps);
         x = (float) (x_zero + v * Math.cos(angulo) * t);
@@ -70,12 +63,17 @@ public class main {
         
         float t;
         int x = 0, y = 0, y_pointer = 280;
+        long tempo;
         boolean sobe = true, game_start = false;
         calculos[6] = angulo_rad(calculos[6]);
         
-        JFrame janela = new JFrame("nome da janela aqui");
+        JFrame janela = new JFrame("Projeto intgrador");
         janela.setSize(800, 600);
         janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
+        
+        button button_class = new button();
+        JButton button = button_class.button();
+        button.setEnabled(true);
         
         JLabel bird = red_bird();
         bird.setBounds(x, y, 32, 32);
@@ -89,16 +87,12 @@ public class main {
         JLabel pointer = pointer();
         pointer.setBounds(20, y_pointer, 28, 7);
         
-        JLabel tela = tela_inicial();
-        tela.setBounds(0, 0, 800, 600);
-        
         JPanel jogo = new JPanel(null);
+        janela.add(button);
         jogo.add(bird);
         jogo.add(pointer);
         jogo.add(barra);
         jogo.add(cenario);
-        jogo.add(tela);
-        
         
         janela.add(jogo);
         janela.setVisible(true);
@@ -135,8 +129,12 @@ public class main {
                 e.printStackTrace();
             
             }
-            //ler o teclado aqui
-       
+            
+            if (b == true) {
+                
+                calculos[4] = (float) (((y_pointer - 280) * (-1)) / 2.4f);
+                game_start = true;
+            }
         }
         
         while (game_start == true) {
@@ -147,8 +145,7 @@ public class main {
             t = calculos[5];
             
             System.out.println("x = " + x + "  y = " + y + "  t = " + t);   //para debug
-            
-            
+                        
             bird.setBounds(x, ((y - 500) * (-1)), 32, 32);
             pointer.setBounds(20, y_pointer, 28, 7);
             barra.setBounds(20, 40, 28, 240);
@@ -160,7 +157,7 @@ public class main {
             jogo.add(cenario);
             jogo.validate();
             
-            if (calculos[4] < 5) {
+            if (calculos[4] < 1) {
                 System.out.println("fim do jogo");
                 System.exit(0);
             }
@@ -171,11 +168,9 @@ public class main {
                 e.printStackTrace();
             
             }
-            
         }
-        
-        
     }
+
     
     public JLabel barra() {
         //carrega a imagem da barra
@@ -197,16 +192,6 @@ public class main {
         return pointer;
     }
     
-    public JLabel tela_inicial() {
-        //carrega a imagem da tela inicial
-        URL url = getClass().getResource("tela.png");
-        Icon icon = new ImageIcon(url);
-        
-        JLabel tela_inicial = new JLabel(icon);
-        
-        return tela_inicial;
-    }
-    
     public JLabel red_bird() {
         //carrga a foto do red bird
         URL url = getClass().getResource("angry-bird.png");
@@ -224,9 +209,44 @@ public class main {
         
         JLabel cenario = new JLabel(icon);
         cenario.setBounds(0, 0, 800, 600);
-        //cenario.setVisible(false);
         
         return cenario;
     }
     
+    public void button_pressed() {
+        b = true;
+    }
+    
+}
+
+class ButtonHandler implements ActionListener {
+	
+    private JButton button;
+	
+    public ButtonHandler(JButton button){
+	
+        this.button = button;
+    }
+	
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        
+        if (evento.getSource() == button) {
+            main main = new main();
+            main.button_pressed();
+        }
+    }
+}
+
+class button{
+    
+    private JButton button = new JButton("");
+    private ButtonHandler handler;
+
+    public JButton button() {
+        
+        handler = new ButtonHandler(button);
+        button.addActionListener(handler);
+        return button;
+    }
 }
